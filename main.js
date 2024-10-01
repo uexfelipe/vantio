1d9cd419f02956a8b23815070c7b5902899b988d
Felipe Passos <berkspar@gmail.com>
2024-09-30 23:09:59 -0300

    all

---
+++ main.js
@@ -0,0 +1,36 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(
+      // 'http://localhost:3000' // Se estiver em desenvolvimento
+      `file://${path.join(__dirname, 'out/index.html')}` // Se estiver em produção
+  );
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
